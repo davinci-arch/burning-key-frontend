544b6afa38ae2431fa6d6c78b5eb70c1e646ac88
Vlad <bondar.vladislav1121@vu.cdu.edu.ua>
2024-05-24 17:27:54 +0300

    added SignIn page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './typing/styles/index.scss'
-import HomePage from './typing/components/HomePage'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './typing/styles/index.scss';
+import HomePage from './typing/components/HomePage';
+import SignPage from './typing/components/SigninPage';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<SignPage />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+);
--- src/typing/components/Header.jsx
+++ src/typing/components/Header.jsx
@@ -1,13 +1,16 @@
+import {Link} from "react-router-dom";
 
 export default function Header() {
 
     return (
         <header className="main-header">
             <div>
+                <Link to="/" className="nav-link">
                 <img src="src/assets/logo.png" alt="logo" className="logo" />
+                </Link>
             </div>
             <div className="user-panel">
-                <span>Sign in</span>
+                <span><Link to="/login" className="nav-link">Sign in</Link></span>
             </div>
         </header>
     )
