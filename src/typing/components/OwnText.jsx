d7c5fcd6539cbc5b1cc9d9d8b2aaf349cbc4bf42
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2024-06-10 04:40:49 +0300

    [fix]: hot fix negative words per minutes, fantastic!

--- src/typing/components/OwnText.jsx
+++ src/typing/components/OwnText.jsx
@@ -3,16 +3,14 @@ import Typing from "./Typing";
 import '../styles/owntext.scss';
 
 export default function OwnText({
-                                         typoRef, isSoundOn, setNewSpeed, newAccuracy, setResult, isDarkTheme, selectedFont, selectedSize,
-                                          setIsReseted, isReseted
-                                     }) {
+    typoRef, isSoundOn, setNewSpeed, newAccuracy, setResult, isDarkTheme, selectedFont, selectedSize,
+    setIsReseted, isReseted
+}) {
     const [isRunning, setIsRunning] = useState(false);
     const [correctKeys, setCorrectKeys] = useState(0);
     const [countKeys, setCountKeys] = useState(0);
     const [isFocused, setFocused] = useState(false);
-    const [isPause, setIsPause] = useState(false);
     const [startTime, setStartTime] = useState();
-    const [pauseTime, setPauseTime] = useState(0);
     const [isTextInputVisible, setIsTextInputVisible] = useState(true);
     const [customText, setCustomText] = useState("");
     const [words, setWords] = useState([]);
@@ -20,29 +18,27 @@ export default function OwnText({
 
     useEffect(() => {
         let timer;
-        if (isRunning && !isPause) {
-            setStartTime(new Date().getTime());
-        } else if (!isRunning && startTime !== undefined && !isPause) {
-            if (!isPause) {
-                if (pauseTime > 0) {
-                    setNewSpeed((correctKeys / 5) / (((((new Date().getTime() - startTime) - pauseTime)) / 1000) / 60));
-                } else {
-                    setNewSpeed((correctKeys / 5) / (((new Date().getTime() - startTime) / 1000) / 60));
-                }
-                newAccuracy((correctKeys / countKeys) * 100)
-                clearInterval(timer);
-                setResult(true);
-                setIsReseted(!isReseted);
-                resetVariables()
+        if (isRunning) {
+            if (startTime == 0) {
+                setStartTime(new Date().getTime());
             }
-
+            setStartTime(new Date().getTime());
+        } else if (!isRunning && startTime > 0) {
+            setNewSpeed((correctKeys / 5) / (((new Date().getTime() - startTime) / 1000) / 60));
+            newAccuracy((correctKeys / countKeys) * 100)
+            clearInterval(timer);
+            setResult(true);
+            setIsReseted(!isReseted);
+            resetVariables();
         }
         return () => clearInterval(timer);
     }, [isRunning]);
 
     const resetVariables = () => {
-        setCountKeys(0)
-        setCorrectKeys(0)
+        setCountKeys(0);
+        setCorrectKeys(0);
+        setStartTime(0);
+        setIsRunning(false);
         if (typoRef.current) {
             typoRef.current.resetText();
         }
@@ -50,29 +46,21 @@ export default function OwnText({
 
     const changeFocuse = () => {
         setFocused(false);
-        if (isRunning) {
-            setIsPause(true);
-            setPauseTime(new Date().getTime());
-        }
     }
 
     const handleFocuse = () => {
         if (typoRef.current) {
             typoRef.current.focusedField()
             setFocused(true);
-            setIsPause(false);
-            if (isRunning) {
-                setPauseTime(new Date().getTime() - pauseTime);
-            }
         }
     }
 
     const handleTextSubmit = () => {
         setIsTextInputVisible(false);
-        if(customText === "")
-        setWords("You have been warned...".match(regex));
+        if (customText === "")
+            setWords("You have been warned...".match(regex));
         else
-        setWords(customText.match(regex));
+            setWords(customText.match(regex));
     }
 
     const handleTextClear = () => {
@@ -93,15 +81,15 @@ export default function OwnText({
             {isTextInputVisible ? (
                 <div className="own-text-container">
                     <div className={`text-input-wrapper ${isDarkTheme ? 'dark' : ''}`}>
-          <textarea
-              value={customText}
-              onChange={handleTextChange}
-              placeholder="Enter your text here..."
-          />
+                        <textarea
+                            value={customText}
+                            onChange={handleTextChange}
+                            placeholder="Enter your text here..."
+                        />
                         <div className="button-container">
-                        <button onClick={handleTextSubmit} style={{marginRight: "3px"}}>Submit</button>
-                        <button onClick={handleTextClear} style={{marginLeft: "3px"}}>Clear</button>
-                    </div>
+                            <button onClick={handleTextSubmit} style={{ marginRight: "3px" }}>Submit</button>
+                            <button onClick={handleTextClear} style={{ marginLeft: "3px" }}>Clear</button>
+                        </div>
                     </div>
                 </div>
             ) : (
@@ -128,11 +116,11 @@ export default function OwnText({
                         >
                             {!isFocused ?
                                 <div className={`description ${isDarkTheme ? 'dark' : ''}`}
-                                     onClick={handleFocuse}
+                                    onClick={handleFocuse}
                                 >
-                <span className={`img-container ${isDarkTheme ? 'dark' : ''}`}>
-                  <img src="/src/assets/cursor.png" alt="cursor" className="cursor-pointer"/>
-                </span>
+                                    <span className={`img-container ${isDarkTheme ? 'dark' : ''}`}>
+                                        <img src="/src/assets/cursor.png" alt="cursor" className="cursor-pointer" />
+                                    </span>
                                     <span className="hint">Click to focus on field</span>
                                 </div> : ""
                             }
